070b0406c5aa1bad99c0c7a13899c964e4c95f0f
Pervs <winfourteen@gmail.com>
2022-12-22 22:29:43 +0800

    First Commit

--- js/index.js
+++ js/index.js
@@ -1,86 +1,18 @@
-//GET request
-// const req = new XMLHttpRequest();
+const addBtn = document.getElementById('addBtn');
 
-// req.open('GET','https://reqres.in/api/users');
+//open the form
+addBtn.addEventListener('click',()=>{
+    const form = document.getElementById('comments');
+    form.style = 'z-index:2; transform: scale(1);';
+});
+//close form
+let exit = document.getElementById('exit').addEventListener('click',()=>{
+    const form = document.getElementById('comments');
+    form.style = 'z-index:0; transform: scale(0);';
+});
 
-// req.addEventListener('load', ()=>{
-//     if(req.status === 200 && req.readyState === 4){
-//         const res = JSON.parse(req.responseText);
-//         console.log(res.data[0].id);
-//     }else{
-//         throw new Error('Bad Request');
-              //or
-            // console.error('Bad Request');
-//     }
 
-    
-// });
-
-// req.send();
-
-/* =========================== */
-//POST req
-
-// const newUser = {
-//     name:'Batman',
-//     sidekick:'Robin'
-// };
-
-// const req = new XMLHttpRequest();
-// req.open('POST','https://reqres.in/api/users');
-// req.setRequestHeader('Content-Type','application/json');
-
-// req.addEventListener('load', ()=>{
-//     if(req.status === 201 && req.readyState === 4){
-//         const res = JSON.parse(req.responseText);
-//         console.log(res);
-//     }else{
-//         // throw new Error('Bad Request');
-//         //       or
-//             console.error('Bad Request');
-//     }
-// });
-// req.send(JSON.stringify(newUser));
-
-/* ========================== */
-
-// PUT Request
-// const newUser = {
-//     name:'Batman',
-//     sidekick:'Robin'
-// };
-
-// const req = new XMLHttpRequest();
-// req.open('PUT','https://reqres.in/api/users/2');
-// req.setRequestHeader('Content-Type','application/json');
-
-// req.addEventListener('load', ()=>{
-//     if(req.status === 200 && req.readyState === 4){
-//         const res = JSON.parse(req.responseText);
-//         console.log(res);
-//     }else{
-//         // throw new Error('Bad Request');
-//         //       or
-//             console.error('Bad Request');
-//     }
-// });
-// req.send(JSON.stringify(newUser));
-
-//DELETE Request
-
-// const req = new XMLHttpRequest();
-
-// req.open('DELETE','https://reqres.in/api/users/2');
-// req.addEventListener('load', ()=>{
-//     if(req.status === 204 && req.readyState === 4){
-//         console.log('Request Successful');
-//     }else{
-//         // throw new Error('Bad Request');
-//         //       or
-//             console.error('Bad Request');
-//     }
-// });
-// req.send();
+let comment = document.getElementById('comment');
 
 const firebaseConfig = {
     apiKey: "AIzaSyC5d8piNDK-4gY9udFFsQYKSr9XQtmnWr4",
@@ -91,36 +23,57 @@ const firebaseConfig = {
     messagingSenderId: "599351448161",
     appId: "1:599351448161:web:00093ab2805013dbc05020"
   };
+
 //initialize firebase
   firebase.initializeApp(firebaseConfig);
 
+
 //reference your database
-let comments = firebase.database().ref('Freedom Wall');
+let comments = firebase.database().ref('/Freedom Wall/' + 'Comments');
 
 document.getElementById('comments').addEventListener('submit',e=>{
     e.preventDefault();
-
-    let user = document.getElementById('user').value;
-    let pass = document.getElementById('password').value;
-    console.log(user,pass);
-
-    saveComments(user,pass);
+    let userComments = comment.value;
+    console.log(userComments);
+    saveComments(userComments);
+    comment.value='';
+    const form = document.getElementById('comments');
+    form.style = 'z-index:0; transform: scale(0);';
 });
 
-const saveComments = (user,pass)=>{
+const saveComments = (userComments)=>{
     comments.push({
-        user:user,
-        pass:pass
+        comment:userComments,
     });
 };
 
 
-//retrieve datas
-const retrieve =()=>{
-    comments.on("value",(data)=>{
-        data.forEach(element => {
-            console.log(element.user);
+//retrieve datas from database
+    comments.on("value",(snapchat)=>{
+        snapchat.forEach(element => {
+            let datas = element.val();
+            console.log(datas);
+            
+            //position setter
+            let positionTop = Math.floor(Math.random()*100);
+            let positionLeft = Math.floor(Math.random()*100);
+            let degreePosition = Math.floor(Math.random()*360);
+            let positionT = positionTop + '%';
+            let positionL = positionLeft + '%';
+            let deg = degreePosition + 'deg';
+            console.log(positionTop,positionLeft);
+
+            const commentContainer = document.getElementById('commentsContainer');
+            const commentBox = document.createElement('div');
+            commentBox.classList.add('comment-box');
+            const comment = document.createElement('p');
+            comment.innerText = datas.comment;
+            comment.classList.add('comment');
+            comment.setAttribute('value',`${comment}`);
+            comment.style = `position: absolute; max-width: 10rem; top:${positionT}; left:${positionL}; transform:rotate(${deg}); overflow-wrap: break-word;`;
+            // const author = document.createElement('p');
+
+            commentContainer.appendChild(commentBox);
+            commentBox.appendChild(comment);
         });
     });
-};
-retrieve();
